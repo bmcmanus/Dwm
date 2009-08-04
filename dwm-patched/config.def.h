cf658219837c2294d9d170489d898c5dec85fed2
Scott Hansen <firecat53@scotty.localdomain>
2009-08-04 16:32:25 -0700

    Changed to bstack as default mode instead of tiling

--- dwm-patched/config.def.h
+++ dwm-patched/config.def.h
@@ -40,10 +40,10 @@ static const Bool resizehints = False; /* False means respect size hints in tile
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "*T*",      tile },    /* first entry is default */
+	{ "*B*",      bstack },         /* first entry is default */
+	{ "*T*",      tile },    	
 	{ "*F*",      NULL },    /* no layout function means floating behavior */
 	{ "*M*",      monocle },
-	{ "*B*",      bstack },
 };
 
 /* key definitions */
@@ -109,10 +109,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_b,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_b,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
