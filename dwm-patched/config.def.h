ae1efcb98dac1150a8578f7023f58f19fcaa8768
firecat53 <firecat53@scotty.localdomain>
2010-01-15 12:13:29 -0800

    Initial commit on flash drive intall

--- dwm-patched/config.def.h
+++ dwm-patched/config.def.h
@@ -23,6 +23,7 @@ static const Rule rules[] = {
 	{ "Inkscape",     NULL,       NULL,        1 << 4,            False,        -1 },
 	{ "Gnome-mplayer",     NULL,       NULL,        1 << 3,            True,        -1 },
 	{ "Mplayer",     NULL,       NULL,        1 << 3,            True,        -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 1,       False,       -1 },
 	{ "Shiretoko",  NULL,       NULL,       1 << 1,       False,       -1 },
 	{ "OpenOffice",  NULL,       NULL,       1 << 6,       False,       -1 },
 	{ NULL,  NULL,       "Terminal",       1 << 0,       False,       -1 },
@@ -63,8 +64,9 @@ static const char *termcmd[]  = { "urxvtc", NULL };
 static const char *termshcmd[]  = { "urxvtc", "-e", "screen", "-d", "-R", "-t", "urxvt", "-S", "terminal", NULL };
 static const char *irssi[]  = { "urxvtc", "-title", "irssi", "-e", "screen", "-d", "-R", "-t", "irssi", "-S", "irssi", "irssi", NULL };
 static const char *wificmd[]  = {  "urxvtc", "-title", "wifi-select", "-e", "sudo","wifi-select", "wlan0", NULL };
-static const char *webcmd[]  = { "firefox", NULL };
-static const char *filemgr[]  = { "urxvtc", "-title", "vifm", "-e", "screen", "-d", "-R", "-t", "vifm", "-S", "vifm", "vifm", NULL };
+static const char *webcmd[]  = { "chromium.sh", NULL };
+static const char *webcmd2[]  = { "firefox", NULL };
+static const char *filemgr[]  = { "urxvtc", "-title", "vifm", "-e", "vifm", NULL };
 static const char *mailcmd[]  = { "urxvtc", "-title", "mutt", "-e","mutt", NULL };
 static const char *musiccmd[]  = { "urxvtc", "-title", "ncmpcpp", "-e","ncmpcpp", NULL };
 static const char *lockcmd[]  = { "lock.sh", NULL };
@@ -77,9 +79,7 @@ static const char *mpd_prev[]  = { "mpc", "prev", NULL };
 static const char *mpd_stop[]  = { "mpc", "stop", NULL };
 static const char *htop[]  = { "urxvtc", "-title", "htop", "-e", "htop", NULL };
 static const char *notes[]  = { "urxvtc", "-title", "Notes", "-e", "vim", "/home/firecat53/docs/notes.txt", NULL };
-static const char *caltodo[]  = { "firefox", "calendar.google.com", NULL };
-static const char *snipload[]  = { "snip", "load", NULL };
-static const char *snipsave[]  = { "snip", "save", NULL };
+static const char *caltodo[]  = { "chromium.sh", "calendar.google.com", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = htop } },
@@ -88,14 +88,13 @@ static Key keys[] = {
 	{ MODKEY,             		XK_x, 	   spawn,          {.v = filemgr } },
 	{ MODKEY|ShiftMask,    		XK_z, 	   spawn,          {.v = termshcmd } },
 	{ MODKEY,             		XK_w, 	   spawn,          {.v = webcmd } },
+	{ MODKEY|ShiftMask,    		XK_w, 	   spawn,          {.v = webcmd2 } },
 	{ MODKEY,             		XK_r, 	   spawn,          {.v = irssi } },
 	{ MODKEY,             		XK_e, 	   spawn,          {.v = mailcmd } },
 	{ MODKEY,             		XK_a, 	   spawn,          {.v = notes } },
 	{ MODKEY,             		XK_i, 	   spawn,          {.v = wificmd } },
 	{ MODKEY,             		XK_g, 	   spawn,          {.v = caltodo } },
 	{ MODKEY,             		XK_n, 	   spawn,          {.v = musiccmd } },
-	{ MODKEY,             		XK_q, 	   spawn,          {.v = snipload } },
-	{ MODKEY|ALTKEY,    		XK_q, 	   spawn,          {.v = snipsave } },
 	{ MODKEY|ALTKEY,      		XK_l, 	   spawn,          {.v = lockcmd } },
 	{ ALTKEY|ControlMask,  		XK_l, 	   spawn,          {.v = lockcmd } },
 	{ 0,  		 		 0x1008ff13, 	   spawn,          {.v = volume_up } },
